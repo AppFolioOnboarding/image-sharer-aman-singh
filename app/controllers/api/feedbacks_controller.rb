cae85998c14f1e6ffc07439c60b97b98c518123b
Aman Singh <aman.singh@appfolio.com>
2020-07-05 20:14:43 -0700

    as - adding create action in api/feedbacks_controller with integration tests

--- app/controllers/api/feedbacks_controller.rb
+++ app/controllers/api/feedbacks_controller.rb
@@ -1,7 +1,26 @@
 module Api
   class FeedbacksController < ApplicationController
+    rescue_from ActionController::ParameterMissing do |e|
+      render(json: e.message.to_json, status: :bad_request)
+    end
+
+    rescue_from do
+      render(body: nil, status: :internal_server_error)
+    end
+
     def create
-      # Implement your create action
+      feedback = Feedback.new(create_feedback_params)
+      if feedback.save
+        head :created
+      else
+        render(json: feedback.errors.to_json, status: :bad_request)
+      end
+    end
+
+    private
+
+    def create_feedback_params
+      { name: params.require(:name), comment: params.require(:comment) }
     end
   end
 end
